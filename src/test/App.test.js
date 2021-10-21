bec9781f2a5232ad7cf9d609c475055e48d84499
devBernardo-Mendes <devbernardomr@gmail.com>
2021-10-21 14:53:11 -0300

    test: :white_check_mark: Test to validate that a transaction has been made

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 import App, { calcularNovoSaldo } from "../App";
 
@@ -25,13 +25,29 @@ describe("Componente principal", () => {
   });
   describe("Quando eu realizo uma transação", () => {
     test("que é um saque, o valor vai diminuir", () => {
-        const valores = {
-          transacao: "saque",
-          valor: 50,
-        };
-        const novoSaldo = calcularNovoSaldo(valores, 150);
+      const valores = {
+        transacao: "saque",
+        valor: 50,
+      };
+      const novoSaldo = calcularNovoSaldo(valores, 150);
 
-        expect(novoSaldo).toBe(100);
-      })
+      expect(novoSaldo).toBe(100);
+    });
+    test("que é um saque, a transaçao deve ser realizada", () => {
+      const { getByText, getByTestId, getByLabelText } = render(<App />);
+
+      const saldo = getByText("R$ 1000");
+      const transacao = getByLabelText("Saque");
+      const valor = getByTestId("valor");
+      const botaotransacao = getByText("Realizar operação");
+
+      expect(saldo.textContent).toBe("R$ 1000");
+
+      fireEvent.click(transacao, { target: { value: "saque" } });
+      fireEvent.change(valor, { target: { value: 10 } });
+      fireEvent.click(botaotransacao);
+
+      expect(saldo.textContent).toBe("R$ 990");
+    });
   });
 });
