650da1f125bf5aac040cd827b0397789ecb9f72e
devBernardo-Mendes <devbernardomr@gmail.com>
2021-10-21 10:56:33 -0300

    test: :white_check_mark: Teste function calcularNovoSaldo

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,37 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 
-import App from "./App";
+import App, { calcularNovoSaldo } from "./App";
 
 describe("Componente principal", () => {
-  it("Mostrar o nome do banco", () => {
-    render(<App />);
-    
-    expect(screen.getByText("ByteBank")).toBeInTheDocument();
+  describe("Quando eu abro o app do banco", () => {
+    test("O nome é exibido", () => {
+      render(<App />);
+
+      expect(screen.getByText("ByteBank")).toBeInTheDocument();
+    });
+
+    test("O saldo é exibido", () => {
+      render(<App />);
+
+      expect(screen.getByText("Saldo:")).toBeInTheDocument();
+    });
+
+    test("O botao de transação é exibido", () => {
+      render(<App />);
+
+      expect(screen.getByText("Realizar operação")).toBeInTheDocument();
+    });
+  });
+  describe("Quando eu realizo uma transação", () => {
+    test("que é um saque, o valor vai diminuir", () => {
+        const valores = {
+          transacao: "saque",
+          valor: 50,
+        };
+        const novoSaldo = calcularNovoSaldo(valores, 150);
+
+        expect(novoSaldo).toBe(100);
+      })
   });
 });
